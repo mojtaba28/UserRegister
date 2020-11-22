1f0c30f4840b2c48cae6f714395af264e07f665d
mojtaba <mojtaba.joshaghani99@gmail.com>
2020-11-23 02:56:46 +0330

    adding google sign in commit

--- android/app/src/main/java/com/example/register/LoginActivity.java
+++ android/app/src/main/java/com/example/register/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.example.register;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,6 +18,13 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.valdesekamdem.library.mdtoast.MDToast;
 
@@ -28,6 +37,9 @@ public class LoginActivity extends AppCompatActivity {
     TextInputEditText email,password;
     Button btn_login;
     SessionManager sessionManager;
+    SignInButton signInButton;
+    GoogleSignInClient googleSignInClient;
+    private static final int RC_SIGN_IN = 12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,15 +61,33 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        //sign in with google account
+
+        GoogleSignInOptions googleSignInOptions=
+                new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleSignInClient= GoogleSignIn.getClient(this,googleSignInOptions);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                googleSignIn();
+            }
+        });
+
 
 
     }
+
 
     public void init(){
         email=findViewById(R.id.edittext_email);
         password=findViewById(R.id.edittext_password);
         tv_register=findViewById(R.id.tv_register);
         btn_login=findViewById(R.id.btn_login);
+        signInButton=findViewById(R.id.btn_google_login);
         tv_forgot_password=findViewById(R.id.tv_forgot_password);
         tv_forgot_password.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -119,5 +149,44 @@ public class LoginActivity extends AppCompatActivity {
         };
         stringRequest.setRetryPolicy(new DefaultRetryPolicy(10000,DefaultRetryPolicy.DEFAULT_MAX_RETRIES,DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         Volley.newRequestQueue(getApplicationContext()).add(stringRequest);
+    }
+
+    private void googleSignIn() {
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            //updateUI(account);
+            sessionManager=new SessionManager(getApplicationContext());
+            sessionManager.setLoggedIn(true);
+            sessionManager.setUserInfo(account.getEmail(),"");
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("signInResult", "signInResult:failed code=" + e.getStatusCode());
+            Toast.makeText(getApplicationContext(), e.getStatusCode(), Toast.LENGTH_LONG).show();
+            //updateUI(null);
+        }
     }
 }
